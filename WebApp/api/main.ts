fd1b52ae4fc4a78ce1d011067b9407f20808289a
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-08 13:06:54 +0200

    back to http for test

--- WebApp/api/main.ts
+++ WebApp/api/main.ts
@@ -1,4 +1,4 @@
-import { serveTLS } from "https://deno.land/std/http/server.ts";
+/*import { serveTLS } from "https://deno.land/std/http/server.ts";
 
 const body = new TextEncoder().encode("Hello HTTPS");
 const options = {
@@ -11,4 +11,11 @@ console.log("Listening...")
 // Top-level await supported
 for await (const req of serveTLS(options)) {
   req.respond({ body });
+}*/
+
+import { serve } from "https://deno.land/std@0.60.0/http/server.ts";
+const s = serve({ port: 8000 });
+console.log("http://localhost:8000/");
+for await (const req of s) {
+  req.respond({ body: "Hello World\n" });
 }
