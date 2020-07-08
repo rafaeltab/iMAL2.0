0af70d9d107c1848ac77e0209c6bf1be5b931b34
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-08 13:13:12 +0200

    global ip instead of local only

--- WebApp/api/main.ts
+++ WebApp/api/main.ts
@@ -15,7 +15,7 @@ for await (const req of serveTLS(options)) {
 
 import { serve } from "https://deno.land/std@0.60.0/http/server.ts";
 const s = serve({ port: 8000 });
-console.log("http://localhost:8000/");
+console.log("http://0.0.0.0:8000/");
 for await (const req of s) {
   req.respond({ body: "Hello World\n" });
 }
