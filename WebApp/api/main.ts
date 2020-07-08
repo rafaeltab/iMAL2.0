fc534933577ac86d9fd1dab9719d5594025207c3
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-08 13:53:47 +0200

    hoping for a miracle at this point

--- WebApp/api/main.ts
+++ WebApp/api/main.ts
@@ -1,6 +1,6 @@
-console.log("hey there");
 import { serve } from "https://deno.land/std@0.60.0/http/server.ts";
-const s = serve({ port: 80, hostname: "imal.ml"});
+const s = serve({ port: 80, hostname: "imal.ml" });
+
 console.log("http://imal.ml/");
 for await (const req of s) {
   req.respond({ body: "Hello World\n" });
