018643ee0a9fdd20b1d2c388e6a3bd041ba46c62
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-08 13:45:05 +0200

    ops 1 to many )

--- WebApp/api/main.ts
+++ WebApp/api/main.ts
@@ -1,4 +1,4 @@
-console.log("hey there"));
+console.log("hey there");
 import { serve } from "https://deno.land/std@0.60.0/http/server.ts";
 const s = serve({ port: 80, hostname: "imal.ml"});
 console.log("http://imal.ml/");
