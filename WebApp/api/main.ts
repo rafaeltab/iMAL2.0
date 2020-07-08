b1b213bf5f50a6ab132bc28f4c6bf541e4fb5a7a
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-08 14:00:05 +0200

    verrry tiny test

--- WebApp/api/main.ts
+++ WebApp/api/main.ts
@@ -1,7 +1,8 @@
-import { serve } from "https://deno.land/std@0.60.0/http/server.ts";
+/*import { serve } from "https://deno.land/std@0.60.0/http/server.ts";
 const s = serve({ port: 80, hostname: "0.0.0.0" });
 
 console.log("http://imal.ml/");
 for await (const req of s) {
   req.respond({ body: "Hello World\n" });
-}
+}*/
+console.log("hey dad");
