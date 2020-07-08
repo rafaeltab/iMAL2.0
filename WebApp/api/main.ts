a560d4e3bb86c7398687806dd1716feb367595b6
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-08 13:29:17 +0200

    0.0.0.0

--- WebApp/api/main.ts
+++ WebApp/api/main.ts
@@ -1,20 +1,5 @@
-/*import { serveTLS } from "https://deno.land/std/http/server.ts";
-
-const body = new TextEncoder().encode("Hello HTTPS");
-const options = {
-  hostname: "localhost",
-  port: 443,
-  certFile: "./certificate.crt",
-  keyFile: "./private.key",
-};
-console.log("Listening...")
-// Top-level await supported
-for await (const req of serveTLS(options)) {
-  req.respond({ body });
-}*/
-
 import { serve } from "https://deno.land/std@0.60.0/http/server.ts";
-const s = serve({ port: 8000 });
+const s = serve({ port: 8000, hostname: "0.0.0.0"});
 console.log("http://0.0.0.0:8000/");
 for await (const req of s) {
   req.respond({ body: "Hello World\n" });
