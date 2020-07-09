1b6afc7ce67ca5e74bb0b34de18ba2ae6dab9ff4
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-09 13:51:27 +0200

    switch to node

--- WebApp/api/main.ts
+++
@@ -1,8 +0,0 @@
-/*import { serve } from "https://deno.land/std@0.60.0/http/server.ts";
-const s = serve({ port: 80, hostname: "0.0.0.0" });
-
-console.log("http://imal.ml/");
-for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
-}*/
-console.log("hey dad");
