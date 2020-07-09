1504b067398f3b4d77cfb7492d8ad8da16974065
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-09 16:47:32 +0200

    listen 0000

--- WebApp/index.js
+++ WebApp/index.js
@@ -4,4 +4,4 @@ const port = process.env.PORT | 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port,"0.0.0.0", () => console.log(`Example app listening at http://0.0.0.0:${port}`))
