8ed932ce7c56db23faba8c794dbf3df412b0a204
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-09 17:06:19 +0200

    minor changes in hope of fixing this issue I got with heroku

--- WebApp/index.js
+++ WebApp/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT | 3000;
-const hostname = process.env.HOST | "0.0.0.0";
+const port = process.env.PORT || 3000;
+const hostname = "0.0.0.0";
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/*', (req, res) => res.send('Hello World!'));
 
 app.listen(port,hostname, () => console.log(`Example app listening at ${hostname}:${port}`));
