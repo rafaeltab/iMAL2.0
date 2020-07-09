5f543dbe43dc2c6dcca6a51ba1593b380c0ca2f1
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-09 23:45:42 +0200

    stringify maybe

--- WebApp/index.js
+++ WebApp/index.js
@@ -4,6 +4,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 const hostname = "0.0.0.0";
 
-app.get('/*', (req, res) => res.send('Hello World!'));
+app.get('/*', (req, res) => {
+    
+    let stringified = JSON.stringify(req.params);
+    res.send(`${req.host}\n${stringified}`);
+});
 
 app.listen(port,hostname, () => console.log(`Example app listening at ${hostname}:${port}`));
