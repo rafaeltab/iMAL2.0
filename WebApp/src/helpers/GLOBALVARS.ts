b5261326b4bee217616eeb4d2299adcab266a7fe
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-10 23:52:13 +0200

    token requesting

---
+++ WebApp/src/helpers/GLOBALVARS.ts
@@ -0,0 +1,7 @@
+const CLIENT_ID = process.env.CLIENT_ID || "noenv";
+const CLIENT_SECRET = process.env.CLIENT_SECRET || "noenv";
+
+const ERROR_STATUS = "error";
+const SUCCESS_STATUS = "success";
+
+export { CLIENT_ID, CLIENT_SECRET, ERROR_STATUS, SUCCESS_STATUS };
