4d9177447e4e1a23d104a8d7d0d9debc870507f1
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-16 00:50:11 +0200

    Added details endpoint

--- WebApp/src/MALWrapper/Anime/Suggestions.ts
+++ WebApp/src/MALWrapper/Anime/Suggestions.ts
@@ -1,52 +1,23 @@
-import { ResponseMessage, ErrorResponse, tokenResponse, RequestResponse } from "../../MALWrapper/BasicTypes";
+import { ResponseMessage, ErrorResponse, tokenResponse, RequestResponse, AnimeNode, ListPagination } from "../../MALWrapper/BasicTypes";
 import { RefreshFetch } from '../../helpers/refresher';
 import { Logger } from '@overnightjs/logger';
 
-type ResponseType = {
-    data: ResponseNode[],
-    paging: {
-        next : string
-    }
-}
-
-type ResponseNode = {
-    node: {
-        id: number,
-        title: string,
-        main_picture: {
-            medium: string,
-            large: string
+export async function GetSuggested(limit: number | undefined, offset: number|undefined, tokens: tokenResponse): Promise<RequestResponse<ListPagination<AnimeNode>>> {
+    let url = `https://api.myanimelist.net/v2/anime/suggestions?limit=${limit ? limit : 10}&offset=${offset ? offset : 0}`;
+    let data = await RefreshFetch(tokens,url, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Bearer ${tokens.access_token}`
         }
+    });
+
+    let newTokens = data.tokens;
+
+    let json: ListPagination<AnimeNode> | ErrorResponse = data.responseJson;
+    if ((json as ErrorResponse).error) {
+        return { response: <ErrorResponse>json };
     }
-}
-
-
-export async function GetSuggested(limit: number | undefined, offset: number|undefined, tokens: tokenResponse): Promise<RequestResponse<ResponseType>> {
-    //try {
-        let url = `https://api.myanimelist.net/v2/anime/suggestions?limit=${limit ? limit : 10}&offset=${offset ? offset : 0}`;
-        let data = await RefreshFetch(tokens,url, {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': `Bearer ${tokens.access_token}`
-            }
-        });
-
-        let newTokens = data.tokens;
-
-        let json: ResponseType | ErrorResponse = data.responseJson;
-        if ((json as ErrorResponse).error) {
-            return { response: <ErrorResponse>json };
-        }
     
-        return { response: { response: (json as ResponseType), tokens: newTokens } };
-    /*} catch (e) {
-        Logger.Info(JSON.stringify(e))
-        return {
-            response: {
-                error: "connection",
-                message: "Error connecting to MyAnimeList"
-            }
-        }
-    }*/
+    return { response: { response: (json as ListPagination<AnimeNode>), tokens: newTokens } };
 }
