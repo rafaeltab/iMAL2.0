b584f92ec556f71cda0b0baed1aaa638501a24d0
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2020-07-21 16:39:25 +0200

    Post instead of Get for login and register and start app login

--- App/iMAL/src/APIManager/Authenticate.ts
+++ App/iMAL/src/APIManager/Authenticate.ts
@@ -1,13 +1,20 @@
-import { AsyncStorage } from 'react-native';
+import { AsyncStorage, Alert } from 'react-native';
 import { isUUID } from './helper/FormatChecker';
+
+type JsonType = {
+    status: "success" | "error",
+    message: string
+}
 
 class Authentication {
     private static instance: Authentication;
-    private stateCode?: string = "12ac8766-cc54-4360-b7c4-91cd1374daec";
+    private stateCode?: string;// = "12ac8766-cc54-4360-b7c4-91cd1374daec";
+    private loaded: boolean = false;
 
     private constructor() {
         console.log("Starting Authenticator...");
-        if(this.stateCode) {
+        if (this.stateCode) {
+            this.loaded = true;
             return;
         }
         
@@ -16,15 +23,59 @@ class Authentication {
             .then((value) => {
                 if (value != null && isUUID(value)) {
                     //value was in localstorage so put it in the variable
+                    this.loaded = true;
                     this.stateCode = value;
-                } else {
-                    //value was not in localstorage so get it from the server
                 }
         });
     }
 
+    private SetCode(uuid: string) {
+        this.loaded = true;
+        this.stateCode = uuid;
+        AsyncStorage.setItem("stateCode", uuid);
+    }
+
+    public getLoaded(): boolean {
+        return this.loaded;
+    }
+
     public GetStateCode(): string | undefined {
         return this.stateCode;
+    }
+
+    /** make request to MAL, check status and save stateCode */
+    public async Trylogin(email: string, password: string): Promise<boolean> {
+        //url to make request to
+        let url = `http://api.imal.ml/authed/login`;
+        //the body of the request
+        let body = {
+            email: email,
+            pass: password
+        }
+        //make the request
+        let res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        });
+        //is the response an error !?!?!?
+        let json: JsonType = await res.json();
+        if (json.status == "error") {
+            //oh fuck
+            Alert.alert("Something bad happened",json.message);
+            return false;
+        }
+
+        //we good
+        if (isUUID(json.message)) {
+            this.SetCode(json.message);
+            return true;
+        }
+        
+        //oh no we not
+        return false;
     }
 
     static getInstance(): Authentication {
--- App/iMAL/src/screens/PreLogin.tsx
+++ App/iMAL/src/screens/PreLogin.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import {Dimensions } from "react-native";
+import { NavigationDrawerScreenProps } from 'react-navigation-drawer';
+import Auth from '../APIManager/Authenticate';
 
-class PreLogin extends React.Component {
-    constructor(props) {
+class PreLogin extends React.Component<NavigationDrawerScreenProps,NavigationDrawerScreenProps> {
+    constructor(props: NavigationDrawerScreenProps) {
         super(props);
+        this.state = props;
+    }
+
+    componentDidMount() {
+        if (Auth.getInstance().getLoaded()) {
+            this.state.navigation.navigate("Main");
+        } else {
+            this.state.navigation.navigate("Login");
+        }   
     }
 
     render() {
         return (
-            <View style={{}}>
-
+            <View style={
+                {
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: Dimensions.get('window').height
+                }}>
+                <Text>Loading</Text>
             </View>
         );
     }
 }
+
+export default PreLogin;
